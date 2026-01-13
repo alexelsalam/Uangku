86d57695f8a1feacd8828bbbfddbfdb37c2d8a8a
alexelsalam <alexelsalam.el@gmail.com>
2026-01-13 21:22:45 +0700

    Add CORS support and update dependencies in server.js and package.json

--- BE/server.js
+++ BE/server.js
@@ -1,8 +1,23 @@
 import express from "express";
-const app = express();
 import transactionsRouter from "./routes/transactions.js";
 import authRouter from "./routes/auth.js";
 import middleware from "./middleware/middleware.js";
+import cors from "cors";
+
+// Create an Express application
+// and configure CORS to allow requests from specific origins
+const app = express();
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173", // dev FE
+      "https://uangku.vercel.app", // prod FE
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use("/", authRouter);
@@ -10,6 +25,10 @@ app.use("/transactions", middleware, transactionsRouter);
 
 app.get("/", (req, res) => {
   res.send("API Catatan Uang berjalan");
+});
+
+app.listen(3000, () => {
+  console.log("Server berjalan di http://localhost:3000");
 });
 
 export default app;
