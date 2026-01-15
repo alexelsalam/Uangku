7b50c35a83fa3851b38b813f9d1b898cda639829
alexelsalam <alexelsalam.el@gmail.com>
2026-01-15 15:52:18 +0700

    add hash pw

--- BE/routes/auth.js
+++ BE/routes/auth.js
@@ -2,6 +2,7 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import pool from "../lib/db.js";
+import bcrypt from "bcryptjs";
 dotenv.config();
 const router = express.Router();
 
@@ -9,13 +10,17 @@ const router = express.Router();
 router.post("/authRegister", async (req, res) => {
   try {
     const { username, password } = req.body;
+
     if (!username || !password) {
       return res
         .status(400)
         .json({ error: "Username and password are required" });
     }
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
     const query = "INSERT INTO users (username, password_hash) VALUES ($1, $2)";
-    await pool.query(query, [username, password]);
+    await pool.query(query, [username, hashedPassword]);
     res.status(201).json({ message: "User created" });
   } catch (err) {
     res.status(500).json({ error: err.message });
@@ -32,14 +37,21 @@ router.post("/authLogin", async (req, res) => {
         .status(400)
         .json({ error: "Username and password are required" });
     }
-    const query =
-      "SELECT * FROM users WHERE username = $1 AND password_hash = $2";
+    const query = "SELECT * FROM users WHERE username = $1";
 
-    const result = await pool.query(query, [username, password]);
+    const result = await pool.query(query, [username]);
     if (result.rows.length === 0) {
       return res.status(401).json({ error: "Invalid credentials" });
     }
     const user = result.rows[0];
+    //  Bandingkan password input dengan hash di database
+    // bcrypt.compare(password_polos, hash_dari_db)
+    const isMatch = await bcrypt.compare(password, user.password_hash);
+
+    if (!isMatch) {
+      return res.status(401).json({ error: "Username atau password salah" });
+    }
+
     const token = jwt.sign(
       { username: user.username },
       process.env.JWT_SECRET,
