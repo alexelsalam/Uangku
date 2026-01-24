287cddb736afd264b88805f528b6d0b6c38c6bab
alexelsalam <alexelsalam.el@gmail.com>
2026-01-24 13:41:30 +0700

    bug token

--- FE/src/components/Logout.jsx
+++ FE/src/components/Logout.jsx
@@ -5,6 +5,13 @@ export default function Logout() {
     localStorage.removeItem("token");
     window.location.href = "/login"; // Redirect to login page
   };
+
+  const token = localStorage.getItem("token");
+  const payload = JSON.parse(atob(token.split(".")[1]));
+  const currentTime = Math.floor(Date.now() / 1000);
+  if (payload.exp < currentTime) {
+    handleLogout(); // Automatically logout if token is expired
+  }
   return (
     <button onClick={handleLogout} className=" ">
       <IconLogOut />
